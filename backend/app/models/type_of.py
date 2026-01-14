786f7aa6636dcd022d5f962f7bb86b29d254fddb
Szabolcs Nagy <n.szaby4@gmail.com>
2026-01-14 09:57:01 +0100

    formatting change

--- backend/app/models/type_of.py
+++ backend/app/models/type_of.py
@@ -8,7 +8,7 @@ from .base import Base
 
 class TypeOf(Base):
     __tablename__ = "type_of"
-    
+
     id: Mapped[Optional[int]] = mapped_column(sa.Integer, primary_key=True)
     request_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("request.id"), nullable=False, index=True)
     request_type_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("request_type.id"), nullable=False)
